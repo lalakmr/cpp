8404a95653c2a96febf75aad598b490cb2994e5f
lalakmr <kerstin79@live.de>
2015-01-18 00:03:43 +0100

    Create hallopaddy.cpp

---
+++ Programmierfortschritte/hallopaddy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	string gruss = "Hallo Paddy";
+	cout << gruss << endl;
+
+	cout << "Sinus von PI: " << sin(3.141592) << endl;
+	return 0;
+}
