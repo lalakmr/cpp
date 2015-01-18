3857abe1b9488a65b0d8dd49f6e99c1793129b01
lalakmr <kerstin79@live.de>
2015-01-18 21:42:04 +0100

    Create showtheunderstanding.cpp

---
+++ Programmierfortschritte/showtheunderstanding.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int x = 4, y = 3, z = 1, p = 0;
+
+	cout << " Wert von y " << y << endl;
+	cout << " Wert von x " << x << endl;
+
+    p =	x * y - z;
+
+	cout << " Wert von p: " << p << endl;
+
+	return 0;
+
+}
