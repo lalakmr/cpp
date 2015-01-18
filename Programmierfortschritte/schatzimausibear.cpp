c5f546f40e14348300d962cc7ebed5c6ab652d1a
lalakmr <kerstin79@live.de>
2015-01-18 21:41:05 +0100

    Create schatzimausibear.cpp

---
+++ Programmierfortschritte/schatzimausibear.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hallo mein herzallerliebstes Engelchen! Es macht mir viel mehr Spass, das hier mit Dir zu lernen, als ich je zu träumen gewagt hätte!" << endl;
+
+	return 0;
